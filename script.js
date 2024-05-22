0a3eade9766325c4a1f9e34d3eb08877f14a009d
Hopedevelopers254 <139498700+Hopedevelopers254@users.noreply.github.com>
2024-05-22 20:13:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const seconds = document.querySelector("#seconds");
 
 
 const currentYear = new Date().getFullYear();
-const newYearTime = new Date(`5 21 ${currentYear} 20:30:00`);
+const newYearTime = new Date(`5 22 ${currentYear} 20:20:00`);
 
 // Update countDownTime
 
